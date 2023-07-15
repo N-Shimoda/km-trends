5c550f61bb98dacf9055db9c6d71644d77114cb5
NAOKI SHIMODA <shimoda.naoki.77s@st.kyoto-u.ac.jp>
2023-07-15 15:15:06 +0900

    finish review2020

--- algorithm/codes/ansCheck2020.c
+++ algorithm/codes/ansCheck2020.c
@@ -5,17 +5,19 @@ void quicksort(int a[], int first, int last);
 int main(void){
 
   int i;
-  int a[8] =  {3,2,6,8,5,1,7,4};
+  int len = 8;
+  int a[8] = {3,2,6,8,5,1,7,4};
+  // int a[8] = {1,4,6,8,2,5,3,7};    // answer for question 3
 
-  for (i = 0; i < 8; i++) {
+  for (i = 0; i < len; i++) {
       printf("array[%d] = %d\n", i, a[i]);
   }
 
   printf("Start sorting...\n");
-  quicksort(a, 0, 7);
+  quicksort(a, 0, len);
 
   printf("Finish!\n");
-  for (i = 0; i < 8; i++) {
+  for (i = 0; i < len; i++) {
       printf("array[%d] = %d\n", i, a[i]);
   }
 
@@ -24,16 +26,14 @@ int main(void){
 
 void quicksort(int a[], int first, int last){
 
-  printf("Check point.");
-
   int i, j, x, t;
+  // printf("saiki\n");   for counting how many times this function was called
 
   x = a[(first+last)/2];
   i = first;  j = last;
   while(1){
     while (a[i]<x) i++;
     while (x<a[j]) j--;
-    printf("(i,j)=(%d,%d)", i, j);
     if (i >= j) break;
     t = a[i];   a[i] = a[j];    a[j] = t;
     i++;  j--;

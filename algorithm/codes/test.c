ee858ead354d2f7093e48e37a9e49b590552f846
NAOKI SHIMODA <shimoda.naoki.77s@st.kyoto-u.ac.jp>
2023-07-15 14:07:49 +0900

    update codes

--- algorithm/codes/test.c
+++ algorithm/codes/test.c
@@ -1,41 +1,7 @@
 #include <stdio.h>
 
-void quicksort(int a[], int first, int last);
-
-int main(void){
-
-  int i;
-  int a[8] =  {3,2,6,8,5,1,7,4};
-
-  for (i = 0; i < 8; i++) {
-      printf("array[%d] = %d\n", i, a[i]);
-  }
-
-  printf("Start sorting...");
-  quicksort(a, 0, 7);
-
-  for (i = 0; i < 8; i++) {
-      printf("array[%d] = %d\n", i, a[i]);
-  }
-
+int main(void) {
+  int i = 9;
+  printf("%f\n", (double)i/(double)2);
   return 0;
-}
-
-void quicksort(int a[], int first, int last){
-
-  printf("Check point.");
-
-  int i, j, x, t;
-
-  x = a[(first+last)/2];
-  i = first;  j = last;
-  while(1){
-    while (a[i]<x) i++;
-    while (x<a[j]) j--;
-    if (i >= j) break;
-    t = a[i];   a[i] = a[j];    a[j] = t;
-    i++;  j--;
-  }
-  if (first < i-1) quicksort(a, first, i-1);
-  if (j+1 < last)  quicksort(a, j+1, last);
 }
